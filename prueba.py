c48a7c02c553465c8bc3ad0caad396e10d9f2d3f
Javier Sanchis Serquo <javier.sanchis@serquo.com>
2023-01-19 08:02:37 +0000

    Upload New File

---
+++ prueba.py
@@ -0,0 +1,20 @@
+# Prueba de automatización Selenium con Python
+# --------------------------------------------
+import os
+import requests
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+script_dir = os.path.dirname(os.path.realpath(__file__))
+chromepath = os.path.join(script_dir, "drivers/chromedriver")
+firefoxpath= os.path.join(script_dir, "drivers/geckodriver")
+class PythonSelenium(unittest.TestCase) :
+    def setUp(self):
+
+    def test_xxxxxx(self):
+
+    def tearDown(self):
+
+if __name__ == "__main__":
+ unittest.main()
+
