b8e32d0277df7763bdb37192044198e8211bc3f4
Javier Sanchis Serquo <javier.sanchis@serquo.com>
2023-02-06 07:50:44 +0000

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -9,13 +9,13 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 chromepath = os.path.join(script_dir, "drivers/chromedriver")
 firefoxpath= os.path.join(script_dir, "drivers/geckodriver")
 class PythonSelenium(unittest.TestCase) :
-    def setUp(self):
+    def setUp(self): # Inicializamos el driver requerido en selenium para la prueba front
 
-    def test_status(self):
+    def test_status(self): # Este test incluye la prueba de back donde comprobamos el código y la fuente de la respuesta.
 
-    def test_buscador(self):    
+    def test_buscador(self): # Este test incluye la prueba donde se navega a través de la web, usando las herramientas de selenium   
 
-    def tearDown(self):
+    def tearDown(self): # Cerramos el driver 
 
 if __name__ == "__main__":
  unittest.main()
